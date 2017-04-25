17ed08e418544f4e58a34f434d424e197fbdb7a0
Walt Sorensen <photodude@users.noreply.github.com>
2017-04-25 10:58:58 -0600

    use docblock commenting style

--- src/phpass/examples/test.php
+++ src/phpass/examples/test.php
@@ -1,11 +1,11 @@
 <?php
-#
-#
-# This is a test program for the portable PHP password hashing framework.
-#
-# Written by Solar Designer and placed in the public domain.
-# See PasswordHash.php for more information.
-#
+/**
+ *
+ * This is a test program for the portable PHP password hashing framework.
+ *
+ * Written by Solar Designer and placed in the public domain.
+ * See PasswordHash.php for more information.
+ */
 
 use openwall\phpass\PasswordHash;
 
@@ -13,8 +13,8 @@ header('Content-type: text/plain');
 
 $ok = 0;
 
-# Try to use stronger but system-specific hashes, with a possible fallback to
-# the weaker portable hashes.
+// Try to use stronger but system-specific hashes, with a possible fallback to
+// the weaker portable hashes.
 $t_hasher = new PasswordHash(8, FALSE);
 $correct = 'test12345';
 $hash = $t_hasher->HashPassword($correct);
@@ -36,7 +36,7 @@ print "Check wrong: '" . $check . "' (should be '0' or '')\n";
 
 unset($t_hasher);
 
-# Force the use of weaker portable hashes.
+// Force the use of weaker portable hashes.
 $t_hasher = new PasswordHash(8, TRUE);
 $hash = $t_hasher->HashPassword($correct);
 
@@ -53,13 +53,13 @@ $check = $t_hasher->CheckPassword($wrong, $hash);
 if (!$check){$ok++};
 print "Check wrong: '" . $check . "' (should be '0' or '')\n";
 
-#
-# A correct portable hash for 'test12345'.
-# Please note the use of single quotes to ensure that the dollar signs will
-# be interpreted literally.  Of course, a real application making use of the
-# framework won't store password hashes within a PHP source file anyway.
-# We only do this for testing.
-#
+/**
+ * A correct portable hash for 'test12345'.
+ * Please note the use of single quotes to ensure that the dollar signs will
+ * be interpreted literally.  Of course, a real application making use of the
+ * framework won't store password hashes within a PHP source file anyway.
+ * We only do this for testing.
+ */
 $hash = '$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0';
 
 print 'Hash: ' . $hash . "\n";
